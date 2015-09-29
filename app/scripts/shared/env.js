cc78af305067fc809661d7145fd8f6d28b13cb7e
chenqi <chenqi@taotaosou.com>
2015-09-29 10:54:32 +0800

    up  server host!

--- app/scripts/shared/env.js
+++ app/scripts/shared/env.js
@@ -5,7 +5,7 @@ let mockHost = "http://" + window.location.hostname + ":8000";
 let ValidIpAddressRegex = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/;
 
 env.services = {
-  main: "http://m.chaoji99.com"
+  main: "http://www.hipac.cn"
 };
 
 env.apis = {
